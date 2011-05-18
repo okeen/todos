c52defa87b66673b491a2eaba3f334df39e45ccd
okeen <eneko.taberna@gmail.com>
2011-05-19 01:52:23 +0200

    Changed root route

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :todos , :through => :todo_lists
   validates :password, :presence => true
 
-  validates :email, :presence => true,
+  validates :email, :presence => true, :uniqueness => true,
     :format   => { :with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
   def as_json(options={})
